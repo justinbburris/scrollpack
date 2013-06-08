9c9873b72e9c5963d78a74083dd4952ec7f881ce
Justin Burris <justinbburris@gmail.com>
2013-06-07 22:14:05 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrollsBuilder::Application.config.secret_token = '20d90f6fa7fa8bf2220a5b51101e4999be9f3d978ef322248864d4af9a747ac5da19ccda85a5ab3630b0ed01a973956604ab9d8fc7600027a7bdf7e1084cce7b'
