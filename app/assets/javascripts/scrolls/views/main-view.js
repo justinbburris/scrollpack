5d5fe270b2569bc890456976ba0ec0007c62c64b
Justin Burris <justinbburris@gmail.com>
2013-06-08 13:46:46 -0400

    Throw in a header.
    Also navigate in a sane manner please

--- app/assets/javascripts/scrolls/views/main-view.js
+++ app/assets/javascripts/scrolls/views/main-view.js
@@ -1,16 +1,7 @@
 Scrolls.Views.MainView = Backbone.View.extend({
 
-  events: {
-    'click a#new-deck': 'createNewDeck'
-  },
-
   initialize: function() {
     this.template = ich.main_view;
-  },
-
-  createNewDeck: function(e) {
-    e.preventDefault();
-    Scrolls.router.navigate('decks/new', {trigger: true});
   },
 
   render: function() {
