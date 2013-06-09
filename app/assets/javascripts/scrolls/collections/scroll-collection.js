55f481a00165b44f106e5efb6deb5f61edc5d661
Justin Burris <justinbburris@gmail.com>
2013-06-09 12:18:23 -0400

    Update filter to act in a way that a user would expect
    E.G. When clicking on a filter, hide all which don't match instead of
    hiding that which does.

--- app/assets/javascripts/scrolls/collections/scroll-collection.js
+++ app/assets/javascripts/scrolls/collections/scroll-collection.js
@@ -12,47 +12,89 @@ Scrolls.Collections.ScrollCollection = Backbone.Collection.extend ({
 
   initialize: function() {
     this.scrollFilter = JSON.parse(JSON.stringify(this.baseFilter));
+    this.noFilter     = true;
+
+    this.on('change:scrollFilter', this.checkFilter);
+  },
+
+  checkFilter: function() {
+    if(JSON.stringify(this.scrollFilter) === JSON.stringify(this.baseFilter)) {
+      this.noFilter = true;
+    } else {
+      this.noFilter = false;
+    }
   },
 
   removeFilter: function(filter_type, filter_name) {
     this.scrollFilter[filter_type] = _.reject(this.scrollFilter[filter_type], function(name) {
       return name === filter_name
     });
+
     this.trigger('change:scrollFilter');
   },
 
   addFilter: function(filter_type, filter_name) {
     this.scrollFilter[filter_type].push(filter_name);
+
     this.trigger('change:scrollFilter');
   },
 
   clearFilter: function() {
     this.scrollFilter = JSON.parse(JSON.stringify(this.baseFilter));
+
     this.trigger('change:scrollFilter');
   },
 
   // Max complexity == n * 10
   // Not great, but considering n == 139, I don't care
   filteredScrolls: function() {
-    var filter = this.scrollFilter;
+    if(this.noFilter) {
+      return this.models;
+    }
 
-    return _.reject(this.models, function(model) {
-      var filtered = false;
+    var filter          = this.scrollFilter;
+    var filteredScrolls = this.models;
+    var typeFilter      = filter.scroll_type;
+    var rarityFilter    = filter.rarity;
+    var resourceFilter  = filter.resource_type;
 
-      _.each(filter.scroll_type, function(scroll_type) {
-        if(model.get('scroll_type') === scroll_type) { filtered = true }
+    if(typeFilter.length > 0) {
+      filteredScrolls = _.filter(filteredScrolls, function(model) {
+        var selected = false;
+
+        _.each(typeFilter, function(scroll_type) {
+          if(model.get('scroll_type') === scroll_type) { selected = true }
+        });
+
+        return selected;
       });
+    }
 
-      _.each(filter.rarity, function(scroll_type) {
-        if(model.get('rarity') === scroll_type) { filtered = true }
+    if(rarityFilter.length > 0) {
+      filteredScrolls = _.filter(filteredScrolls, function(model) {
+        var selected = false;
+
+        _.each(rarityFilter, function(scroll_type) {
+          if(model.get('rarity') === scroll_type) { selected = true }
+        });
+
+        return selected;
       });
+    }
 
-      _.each(filter.resource_type, function(scroll_type) {
-        if(model.get('resource_type') === scroll_type) { filtered = true }
+    if(resourceFilter.length > 0) {
+      filteredScrolls = _.filter(filteredScrolls, function(model) {
+        var selected = false;
+
+        _.each(resourceFilter, function(scroll_type) {
+          if(model.get('resource_type') === scroll_type) { selected = true }
+        });
+
+        return selected;
       });
+    }
 
-      return filtered;
-    });
+    return filteredScrolls;
   }
 
 });
