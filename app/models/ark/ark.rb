644d5f0b37ab1eefb81e1a25ef6ae32c7999fa76
Justin Burris <justinbburris@gmail.com>
2013-06-09 22:43:40 -0400

    My really shitty image grabber (don't use it)

--- app/models/ark/ark.rb
+++ app/models/ark/ark.rb
@@ -1,4 +1,5 @@
 require 'digest/md5'
+require 'fileutils'
 
 class Ark::Ark
   @@URL = 'http://www.scrollsguide.com/wiki/index.php/Scrolls_Database'
@@ -24,14 +25,44 @@ class Ark::Ark
       scrolls = ScrollParser.new(checksum_registry.html_data).parse
 
       @scroll_model_class.transaction do
-        scrolls.each { |scroll| @scroll_model_class.create!(scroll) }
+        scrolls.each { |scroll| @scroll_model_class.create!(scroll) } unless @dev
         checksum_registry.add unless (@force or @dev)
       end
+    end
+
+    # fetch_images('http://theyseemescrollin.com/images/scrolls')
+
+  end
+
+  def fetch_images(image_url)
+    @scroll_model_class.all.each do |scroll|
+      # so we pull down the html
+      response = HTTParty.get("#{image_url}/#{scroll.url_name}.png")
+
+      create_image("app/assets/images/scrolls/#{scroll.scroll_id}_#{scroll.url_name}.png", response)
     end
   end
 
   def flood
     @scroll_model_class.destroy_all
     @parsed_registry_class.destroy_all
+  end
+
+  private
+
+  def create_image(path, image)
+    dir = File.dirname(path)
+
+    unless File.directory?(dir)
+      FileUtils.mkdir_p(dir)
+    end
+
+    unless File.exist?(path)
+      puts "Saving Image : #{path}"
+      File.open(path, 'wb') do |f|
+        f << image
+      end
+    end
+    
   end
 end
